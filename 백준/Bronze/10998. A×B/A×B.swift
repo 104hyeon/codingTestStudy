57f51cd24b12e4f828e9d53fba5d45edb63fde72
104hyeon <104hyeon@gmail.com>
2025-08-28 20:36:34 +0900

    Update A×B.swift

--- 백준/Bronze/10998. A×B/A×B.swift
+++ 백준/Bronze/10998. A×B/A×B.swift
@@ -1,2 +1,9 @@
 var numbers = readLine()!.split(separator: " ").map { Int(String($0))! }
+
 print(numbers[0] * numbers[1])
+
+
+/*
+덧셈 뺄셈 때 썼던게 계속 오류가 나서 구글링해서 찾아봄
+map 함수로 변환해서 첫 번째 숫자와 두 번째 숫자 곱하는 걸로 변경함
+*/
